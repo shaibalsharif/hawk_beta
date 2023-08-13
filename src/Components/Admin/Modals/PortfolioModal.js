c789ecc76165b5460102c0da28841fcfa54570fe
Shaibal Sharif <shaibalsharif@gmail.com>
2023-08-13 18:27:17 +0600

    Add files via upload

--- src/Components/Admin/Modals/PortfolioModal.js
+++ src/Components/Admin/Modals/PortfolioModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { getEmbedLink } from '../../../Firebase/firebase_utils';
 
 const PortfolioModal = ({ data, onClose }) => {
 
@@ -45,173 +46,187 @@ const PortfolioModal = ({ data, onClose }) => {
     };
 
     return (
-        <div className="absolute w-full flex justify-center items-center bg-black bg-opacity-80 overflow-y-scroll top-0 left-0 ">
-            <div className="bg-dark-2 p-8 rounded-lg w-11/12 md:w-9/12 lg:w-7/12 xl:w-5/12">
-                <h2 className="text-xl font-semibold mb-4">{data ? 'Edit Portfolio' : 'Add Portfolio'}</h2>
-                <form onSubmit={handleSubmit}>
+        <div className="absolute w-full min-h-screen   bg-black bg-opacity-80 overflow-y-scroll top-0 left-0 ">
+            <div className='h-4/5 flex justify-center gap-2 mt-24 items-center'>
+                <div className="bg-dark-2 p-8 rounded-lg w-11/12 md:w-9/12 lg:w-7/12 xl:w-5/12">
+                    <h2 className="text-xl font-semibold mb-2">{data ? 'Edit Portfolio' : 'Add Portfolio'}</h2>
+                    <form onSubmit={handleSubmit}>
 
-                    {/* COVER */}
-                    <div className='grid grid-cols-3 md:grid-cols-5 gap-2'>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Cover Title:</label>
-                            <input
-                                type="text"
-                                value={coverTitle}
-                                onChange={(e) => setCoverTitle(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Cover Title"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Client:</label>
-                            <input
-                                type="text"
-                                value={client}
-                                onChange={(e) => setClient(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Client"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Year:</label>
-                            <input
-                                type="text"
-                                value={year}
-                                onChange={(e) => setYear(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Year"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Role:</label>
-                            <input
-                                type="text"
-                                value={role}
-                                onChange={(e) => setRole(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Role"
-                            />
-                        </div>
-                    </div>
-
-
-
-                    {/* COVER */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Content Description:</label>
-                        <textarea
-                            value={contentDescription}
-                            onChange={(e) => setContentDescription(e.target.value)}
-                            className="border rounded px-3 py-2 w-full bg-dark-2 bg-dark-2"
-                            placeholder="Content Description"
-                        />
-                    </div>
-
-                    {/* INNER PAGE */}
-                    <div className="mb-4">
-                        <label className="block font-medium mb-1">Points:</label>
+                        {/* COVER */}
                         <div className='grid grid-cols-3 md:grid-cols-5 gap-2'>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Cover Title:</label>
+                                <input
+                                    type="text"
+                                    value={coverTitle}
+                                    onChange={(e) => setCoverTitle(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Cover Title"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Client:</label>
+                                <input
+                                    type="text"
+                                    value={client}
+                                    onChange={(e) => setClient(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Client"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Year:</label>
+                                <input
+                                    type="text"
+                                    value={year}
+                                    onChange={(e) => setYear(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Year"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Role:</label>
+                                <input
+                                    type="text"
+                                    value={role}
+                                    onChange={(e) => setRole(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Role"
+                                />
+                            </div>
+                        </div>
 
-                            {points.map((point, index) => (
-                                <div key={index} className="flex items-center space-x-2 mb-2 relative">
-                                    <input
-                                        type="text"
-                                        value={point}
-                                        onChange={(e) => {
-                                            const updatedPoints = [...points];
-                                            updatedPoints[index] = e.target.value;
-                                            setPoints(updatedPoints);
-                                        }}
-                                        className=" pl-2 pr-10 py-1 w-full bg-dark-2 outline-none "
-                                        placeholder={`Point ${index + 1}`}
-                                    />
-                                    <button
-                                        type="button"
-                                        onClick={() => handleDeletePoint(index)}
-                                        className="text-red-600 absolute  right-2 hover:bg-slate-500 p-0"
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                                         className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
 
-                                    </button>
-                                </div>
-                            ))}
+
+                        {/* COVER */}
+                        <div className="mb-2">
+                            <label className="block font-medium mb-1">Content Description:</label>
+                            <textarea
+                                value={contentDescription}
+                                onChange={(e) => setContentDescription(e.target.value)}
+                                className="border rounded px-3 py-2 w-full bg-dark-2 bg-dark-2"
+                                placeholder="Content Description"
+                            />
+                        </div>
+
+                        {/* INNER PAGE */}
+                        <div className="mb-2">
+                            <label className="block font-medium mb-1">Points:</label>
+                            <div className='grid grid-cols-3 md:grid-cols-5 gap-2'>
+
+                                {points.map((point, index) => (
+                                    <div key={index} className="flex items-center space-x-2 mb-2 relative">
+                                        <input
+                                            type="text"
+                                            value={point}
+                                            onChange={(e) => {
+                                                const updatedPoints = [...points];
+                                                updatedPoints[index] = e.target.value;
+                                                setPoints(updatedPoints);
+                                            }}
+                                            className=" pl-2 pr-10 py-1 w-full bg-dark-2 outline-none "
+                                            placeholder={`Point ${index + 1}`}
+                                        />
+                                        <button
+                                            type="button"
+                                            onClick={() => handleDeletePoint(index)}
+                                            className="text-red-600 absolute  right-2 hover:bg-slate-500 p-0"
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                                                className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                            </svg>
+
+                                        </button>
+                                    </div>
+                                ))}
+                                <button
+                                    type="button"
+                                    onClick={handleAddPoint}
+                                    className="text-green-600"
+                                >
+                                    Add Point
+                                </button>
+                            </div>
+
+                        </div>
+
+
+
+                        {/* CREATOR SECTION */}
+
+                        <div className='grid grid-cols-2 md:grid-cols-3 gap-2'>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Creator Section - Company:</label>
+                                <input
+                                    type="text"
+                                    value={creatorSectionCompany}
+                                    onChange={(e) => setCreatorSectionCompany(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Company"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Creator Section - Description:</label>
+                                <textarea
+                                    value={creatorSectionDescription}
+                                    onChange={(e) => setCreatorSectionDescription(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2 bg-dark-2"
+                                    placeholder="Description"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Creator Section - Name:</label>
+                                <input
+                                    type="text"
+                                    value={creatorSectionName}
+                                    onChange={(e) => setCreatorSectionName(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Name"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label className="block font-medium mb-1">Creator Section - Position:</label>
+                                <input
+                                    type="text"
+                                    value={creatorSectionPosition}
+                                    onChange={(e) => setCreatorSectionPosition(e.target.value)}
+                                    className="border rounded px-3 py-2 w-full bg-dark-2"
+                                    placeholder="Position"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="mt-4 flex justify-between">
                             <button
                                 type="button"
-                                onClick={handleAddPoint}
-                                className="text-green-600"
+                                onClick={onClose}
+                                className="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400 focus:outline-none"
                             >
-                                Add Point
+                                Cancel
+                            </button>
+                            <button
+                                type="submit"
+                                className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 focus:outline-none"
+                            >
+                                {data ? 'Save Changes' : 'Add Portfolio'}
                             </button>
                         </div>
-
+                    </form>
+                </div>
+                <div className='bg-dark-2 w-[20%] h-[70vh] rounded-md overflow-hidden overflow-y-scroll'>
+                    <h2 className="text-xl font-semibold p-4">{'Images'}</h2>
+                 
+                    {data.content_list.length ? (<div className='p-2 space-y-4 last:mb-8'>
+                        {data.content_list.map(item => <img className='border p-2' src={item.type=='g-drive'?getEmbedLink(item.url):item.url}/>)}
+                    </div>) : <></>}
+                    <div className='p-2 w-full'>
+                       <input type='file' className='w-full'/> 
                     </div>
-
-
-
-                    {/* CREATOR SECTION */}
-
-                    <div className='grid grid-cols-2 md:grid-cols-3 gap-2'>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Creator Section - Company:</label>
-                            <input
-                                type="text"
-                                value={creatorSectionCompany}
-                                onChange={(e) => setCreatorSectionCompany(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Company"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Creator Section - Description:</label>
-                            <textarea
-                                value={creatorSectionDescription}
-                                onChange={(e) => setCreatorSectionDescription(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2 bg-dark-2"
-                                placeholder="Description"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Creator Section - Name:</label>
-                            <input
-                                type="text"
-                                value={creatorSectionName}
-                                onChange={(e) => setCreatorSectionName(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Name"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block font-medium mb-1">Creator Section - Position:</label>
-                            <input
-                                type="text"
-                                value={creatorSectionPosition}
-                                onChange={(e) => setCreatorSectionPosition(e.target.value)}
-                                className="border rounded px-3 py-2 w-full bg-dark-2"
-                                placeholder="Position"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="mt-4 flex justify-between">
-                        <button
-                            type="button"
-                            onClick={onClose}
-                            className="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400 focus:outline-none"
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="submit"
-                            className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 focus:outline-none"
-                        >
-                            {data ? 'Save Changes' : 'Add Portfolio'}
-                        </button>
-                    </div>
-                </form>
+                    
+                </div>
             </div>
+
         </div>
     );
 };
